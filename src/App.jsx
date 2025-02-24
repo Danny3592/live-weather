485ec24717d99d51738ee78c5f9d3710972efc87
danny3592 <dannyxiao15@gmail.com>
2025-02-24 10:18:45 +0800

    搬移路由

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
+import { StrictMode } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import MainHeader from './layout/MainHeader.jsx';
+import Home from './pages/Home.jsx';
+import WeatherApp from './pages/weatherApp.jsx';
+import Login from './pages/Login.jsx';
+import { Provider } from 'react-redux';
+import store from './store/store.jsx';
+
+// 設定路由
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/user',
+    element: <MainHeader />,
+    children: [
+      {
+        path: 'weather',
+        element: <WeatherApp />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+    ],
+  },
+]);
+
 const App = () => {
-  return <div></div>;
+  return (
+    <Provider store={store}>
+      <RouterProvider router={routes} />
+    </Provider>
+  );
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,43 +1,10 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App.jsx';
-
-import { createBrowserRouter, RouterProvider } from 'react-router';
-import MainHeader from './layout/MainHeader.jsx';
-import Home from './pages/Home.jsx';
-import WeatherApp from './pages/weatherApp.jsx';
-import Login from './pages/Login.jsx';
-import { Provider } from 'react-redux';
-import store from './store/store.jsx';
-
-const routes = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/user',
-    element: <MainHeader />,
-    children: [
-      {
-        path: 'weather',
-        element: <WeatherApp />,
-      },
-      {
-        path: 'login',
-        element: <Login />,
-      },
-    ],
-  },
-]);
+import './index.css';
+import { StrictMode } from 'react';
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <RouterProvider router={routes}>
-      <StrictMode>
-        <App />
-      </StrictMode>
-    </RouterProvider>
-  </Provider>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
