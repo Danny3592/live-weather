4abfcce05bb6c7bf3a65e9c92bc55f3cd3118c6c
danny3592 <dannyxiao15@gmail.com>
2025-02-24 10:43:09 +0800

    調整圖片

--- src/pages/WeatherApp.jsx
+++ src/pages/WeatherApp.jsx
@@ -191,11 +191,11 @@ const WeatherApp = () => {
             </div>
 
             <div className="right  flex flex-col gap-5  w-2/3 justify-center items-center h-full pt-5">
-              <div  className='w-[450px]  p-10 rounded-xl bg-[#ffffff6a]'>
+              <div  className='w-[450px] h-full p-10 rounded-xl bg-[#ffffff6a] my-10'>
                 <img
                   src={`../../public/images/${city}.jpg`}
                   alt=""
-                  className="w-full rounded-xl " 
+                  className="w-full h-full rounded-xl " 
                 />
               </div>
             </div>
